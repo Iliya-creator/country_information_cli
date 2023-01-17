61755cdac78658d91582888bb379c5139b9306a3
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2023-01-16 20:27:44 -0800

    update
    I added program flow to this app

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,28 @@
 class CLI
     def run
-      puts "Greetings"
+        system('clear')
+        greeting
+        instructions_prompt
+        program_runs
+        end_program
+
+    end
+
+    def greeting
+        puts "Welcome to Countries of the World."
+    end
+
+    def instructions_prompt
+        puts "Just follow along with the instructions and prompts."
+    end
+
+    def program_runs
+        puts "Program runs........."
+    end
+
+    def end_program
+        puts "See you later. To run the application again, open WSL, cd into '/mnt/c/Users/Matve/Desktop/country_information_cli' and then run 'ruby bin/tracker'"
     end
   end
+
+
