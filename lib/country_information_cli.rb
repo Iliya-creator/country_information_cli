9706e95abfcd907887b4b43f3079e9657a1852bb
Iliya-creator <78891186+Iliya-creator@users.noreply.github.com>
2023-01-16 18:35:19 -0800

    Initial commit
    Starting out on this project

---
+++ lib/country_information_cli.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "country_information_cli/version"
+
+module CountryInformationCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
